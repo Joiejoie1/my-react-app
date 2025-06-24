498d1e124b335bd14359b3cfabb31957ec9b1aa0
Joy <soniqjjj@gmail.com>
2025-06-24 15:15:08 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const Greetings = () => <p>Hello there! Good morning</p>;
+
+
+// Avatar component....
+const Avatar = () => (
+  <div className="card-image">
+    <img
+      className="card-image"
+      src="https://www.w3schools.com/howto/img_avatar.png"
+      alt="Avatar"
+      style={{ width: 100 }}
+    />
+  </div>
+);
+
+// Profile card....
+const UserProfileCard = () => (
+
+  <div className="card">
+    <Avatar/>
+    <div className="card-name">Joy</div>
+    <div>Joy is a Frontend Developer</div>
+  </div>
+);
+
+// A list item component....
+const ListItem = (props) => (
+  <li>
+    Ford
+    Tesla
+    Volvo
+    Mercedes
+    Fiat
+  </li>
+)
+
+const App = () => {
+  const items = ["Ford", "Tesla", "Volvo", "Mercedes", "Fiat"];
+
+  const ListItems = items.map(item => <li key={item}>{item}</li>);
+
   return (
-    <div className="App">
-      <h1>Hello, Joiejoie1!</h1>
-      <p>Welcome to your first React app in GitHub Codespaces 🎉</p>
-    </div>
+    <ol>
+      {ListItems}
+    </ol>
   );
-}
+};
 
 export default App;
